dd2d67f08231d2d7b5f9014e39fff6ccd2400937
Ernest <chancedelaware@gmail.com>
2023-05-05 20:01:30 -0400

    added subnet

--- Dev-VPC.tf
+++ Dev-VPC.tf
@@ -4,5 +4,14 @@ resource "aws_vpc" "Development-vpc" {
   tags = {
     name = "Development-envn-vpc"
   }
+  
+}
 
+resource "aws_subnet" "Development-subnet-1" {
+  cidr_block = "10.2.0.0/24"
+  vpc_id = aws_vpc.Development-vpc.id
+  availability_zone = "us-east-1a"
+  tags = {
+    name = "Development-envn-subnet-1"
+  }
 }
